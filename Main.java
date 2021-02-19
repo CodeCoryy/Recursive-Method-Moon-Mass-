75535933400100240892bf4e7887caa7619dec35
newtampacperson <cory.person@patelhs.org>
2021-02-19 17:37:59 +0000

    Settled on altitude idea. Finished everythig today (Implemented everything from rubric)

--- Main.java
+++ Main.java
@@ -1,23 +1,36 @@
+import java.util.Scanner;
+//Scanner ^
 import java.util.concurrent.TimeUnit;
+//Time addition. Makes everything smooth and more ralistic. ^
 class Main {
   public static void main(String[] args) throws InterruptedException {
-    System.out.println("How much do you weigh on the moon lmao");
-    liftOff(3);
-  }
-  public static liftOff(int countdown) throws InterruptedException {
-    if(countdown == 0){
-      System.out.print("\nLift Off!\n\n");
+    System.out.print("How many miles do you want to be from the moon?\n\n");
+    Scanner sc = new Scanner(System.in);
+    int startDistance = sc.nextInt();
+    //Requirements in order for function to work as intended 
+    if(startDistance > 500 && startDistance % 500 == 0) {
+      System.out.println("\nWe are at a approxmate distance of " + startDistance + " miles towards the moon. Operation Landship activated - over \n");
+      //Method call
+      land(startDistance);
     }
+    else
+    {
+      System.out.println("\nOperation Landship Deactivated. Altitude requirements not sufficient (Make it by every 500!! Also no negatives please)");
+    }
+  }
+  public static int land(int altDrop) throws InterruptedException {
+    //Once altitude is 0, you land. Base case. 
+    if(altDrop == 0){
+      System.out.println("\nWe have approached the surface of the moon!");
+      return altDrop;
+    }
+    //recursve part. Counts down by 500 miles (in altitude) till your altitude is 0 from the moon. 
     else{
-      System.out.println(countdown);
+      System.out.println(altDrop);
       TimeUnit.SECONDS.sleep(1);
-      countdown --;
-      liftOff(countdown);
-      return idk;
+      altDrop -= 500;
+      return land(altDrop);
     }
   }
 }
-
-
-
 //hi
